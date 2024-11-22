1456214286fa6dda833980b0782ef5fa71d5372d
Tbernier01 <43450054+Tbernier01@users.noreply.github.com>
2024-11-22 17:36:10 +0100

    Update Step_1.sql
    Test SQL

--- Databricks/Step_1.sql
+++ Databricks/Step_1.sql
@@ -1,1 +1,41 @@
 --Test--
+
+WITH FilteredSales AS (
+    --Step 1--
+  SELECT 
+        soh.TerritoryID AS StoreID,
+        sod.ProductID,
+        sod.LineTotal,
+        sod.OrderQty,
+        sod.UnitPrice,
+        YEAR(soh.OrderDate) AS OrderYear
+    FROM 
+        Sales.SalesOrderHeader soh
+  --Step 2--  
+  INNER JOIN 
+        Sales.SalesOrderDetail sod
+    ON 
+       --step 3--
+  soh.SalesOrderID = sod.SalesOrderID
+    WHERE 
+  --step 4--
+        YEAR(soh.OrderDate) = YEAR(GETDATE()) -- Filter current year
+),
+  --step 5--
+AggregatedSales AS (
+    SELECT 
+        StoreID,
+        ProductID,
+        SUM(LineTotal) AS TotalSalesAmount,
+        SUM(OrderQty) AS TotalQuantity,
+        AVG(UnitPrice) AS AvgPrice
+    FROM 
+        FilteredSales
+    GROUP BY 
+        StoreID, ProductID
+)
+INSERT INTO Sales.ProductStoreSummary (StoreID, ProductID, TotalSalesAmount, TotalQuantity, AvgPrice)
+SELECT 
+    StoreID, ProductID, TotalSalesAmount, TotalQuantity, AvgPrice
+FROM 
+    AggregatedSales;
